eacf355f65c10722aade70f73ccc2d4fa28ad0c1
Sea <hjw_redeption@outlook.com>
2015-11-05 19:54:57 -1200

    some progess on indexmanager

--- src/Index/IndexManager.cpp
+++ src/Index/IndexManager.cpp
@@ -16,19 +16,20 @@ IndexManager* IndexManager::getInstance(){
 	return instance;
 }
 
-Index IndexManager::createIndex(std::string name, Schema *s, std::string columnName, int type){
-	if (!s->isAttrExists(columnName))
-		throw std::runtime_error("this column doesn't exist!");
-	if (!s->isUnique(columnName))
-		throw std::runtime_error("this attribute is not unique.");
-	s->addIndex(columnName);
+Index* IndexManager::createIndex(std::string name, Schema *s, std::string columnName, int type){
+	// if (!s->isAttrExists(columnName))
+	// 	throw std::runtime_error("this column doesn't exist!");
+	// if (!s->isUnique(columnName))
+	// 	throw std::runtime_error("this attribute is not unique.");
+	// s->addIndex(columnName);
 	BufferManager* bm = BufferManager::getInstance();
-	bm->write("index::" + name, 0, -1);
-	bm->write("index::" + name, 0);
-	return Index(name, Schema::BTREE, s->getLength(name));
+	if (bm->isFileExists("index-" + name + ".wk"))
+		throw std::runtime_error("index already exists");
+		
+	return new Index(name, Schema::BTREE, s->getLength(name));
 }
 
-Index IndexManager::getIndex(std::string name){
+Index* IndexManager::getIndex(std::string name){
 	
 }
 
