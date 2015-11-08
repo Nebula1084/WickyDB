8f57b78525af6eaa706b6c80ffba44ea4c844fb7
FrozenShore <Cyua@FrozenShore.local>
2015-11-08 10:51:27 +0800

    fix some print bug

--- src/Record/Table.cpp
+++ src/Record/Table.cpp
@@ -20,12 +20,12 @@ bool Table::CreateTable(std::vector<Attribute> v){
 
 void Table::printTable(){
 	for(int i=0; i<attrList.size(); i++){
-		std::cout<<std::setw(10)<<attrList[i].getName();
+		std::cout<<std::setw(12)<<attrList[i].getName();
 	}
 	std::cout<<std::endl;
 	for(int i=0; i<rows.size(); i++){
 		for(int j=0; j<rows[i].col.size(); j++){
-			std::cout<<std::setw(10)<<rows[i].col[j];
+			std::cout<<std::setw(12)<<rows[i].col[j];
 		}
 		std::cout<<std::endl;
 	}
